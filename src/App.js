51ae70f7ed482975a8a17e88dfbf8c7cb7a0a946
Sandruttee <sandra.placenyte@gmail.com>
2023-08-09 21:00:10 +0200

    Added a git link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather application</div>;
+  return (
+    <div className="App">
+      <h1>Weather application</h1>
+      <footer>
+        This project was coded by Sandra Plačenytė and is{" "}
+        <a
+          href="https://github.com/Sandruttee/my-weather-application"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
