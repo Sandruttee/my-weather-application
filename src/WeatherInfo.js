a44297d0d237b543ae11870b6df1e24725adfd27
Sandruttee <sandra.placenyte@gmail.com>
2023-08-12 23:31:17 +0200

    Added a weather icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,7 +13,7 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <img src={props.data.icon} alt={props.data.description} />{" "}
+          <img src={props.data.iconUrl} alt={props.data.description} />{" "}
           <span className="temperature">
             {Math.round(props.data.temperature)}
           </span>
