481af6af4f7aad42b7b7a77445085a005487723d
Sandruttee <sandra.placenyte@gmail.com>
2023-08-15 10:05:15 +0200

    made unit conversion work

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function WeatherTemperature() {
-  return (
-    <div>
-      <span className="temperature">{Math.round(props.data.temperature)}</span>
-      <span className="unit">°C</span>{" "}
-    </div>
-  );
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
+  function convertToFah(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function convertToCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  if (unit === "celsius") {
+    return (
+      <div>
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="#" onClick={convertToFah}>
+            °F{" "}
+          </a>
+        </span>{" "}
+      </div>
+    );
+  } else {
+    let fahTemp = (props.celsius * 9) / 5 + 32;
+    return (
+      <div>
+        <span className="temperature">{Math.round(fahTemp)}</span>
+        <span className="unit">
+          <a href="#" onClick={convertToCelsius}>
+            °C{" "}
+          </a>
+          | °F{" "}
+        </span>{" "}
+      </div>
+    );
+  }
 }
