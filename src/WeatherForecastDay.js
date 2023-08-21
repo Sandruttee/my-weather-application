417d5bb51122d44c9225ec62f4651205c74e6234
Sandruttee <sandra.placenyte@gmail.com>
2023-08-21 21:14:12 +0200

    created a forecast for one day

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,15 +1,32 @@
 import React from "react";
 
 export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+
   return (
     <div>
-      <div className="WeatherForecast-day"> {props.data.dt}</div>
+      <div className="WeatherForecast-day"> {day()}</div>
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperatures-max">
-          {Math.round(props.data.temp.max)}°
+          {maxTemperature()}
         </span>
         <span className="WeatherForecast-temperatures-min">
-          {Math.round(props.data.temp.min)}°
+          {minTemperature()}
         </span>
       </div>{" "}
     </div>
